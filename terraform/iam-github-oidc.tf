92701e97da11bc4c10d280f707d4cc198b833944
Tafadzwa <tdzomba@gmail.com>
2026-08-29 15:09:06 +0200

    Fix GitHub OIDC trust policy

--- terraform/iam-github-oidc.tf
+++ terraform/iam-github-oidc.tf
@@ -38,7 +38,7 @@ data "aws_iam_policy_document" "github_actions_trust" {
     }
 
     condition {
-      test     = "StringEquals"
+      test     = "StringLike"
       variable = "token.actions.githubusercontent.com:sub"
 
       values = [
