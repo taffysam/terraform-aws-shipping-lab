ea7fc53fcd5fa4660dc1f68cf7a8bda4af10e235
Tafadzwa <tdzomba@gmail.com>
2026-08-29 15:34:56 +0200

    Update OIDC trust policy for immutable GitHub identity

--- terraform/iam-github-oidc.tf
+++ terraform/iam-github-oidc.tf
@@ -38,7 +38,7 @@ data "aws_iam_policy_document" "github_actions_trust" {
     }
 
     condition {
-      test     = "StringLike"
+      test     = "StringEquals"
       variable = "token.actions.githubusercontent.com:sub"
 
       values = [
